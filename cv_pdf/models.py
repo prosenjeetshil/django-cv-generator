ba941e5bf5cffdcdef6944b7ffc5ef1fedd33505
prosenjeetshil <prosenjeetshil@gmail.com>
2025-03-02 14:30:52 +0530

    feat: add Profile model with required fields

--- cv_pdf/models.py
+++ cv_pdf/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    summary = models.TextField(max_length=2000)
+    degree = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    university = models.CharField(max_length=200)
+    previous_work = models.TextField(max_length=1000)
+    skills = models.TextField(max_length=1000)
+
+    # viki - 12345
